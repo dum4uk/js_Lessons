8cddb2a6f6d0e7afea788f52dcd747c3b648499f
dum4uk <89747200+dum4uk@users.noreply.github.com>
2025-01-30 14:23:13 +0200

    Add files via upload

--- lesson_2/js/task_4.js
+++ lesson_2/js/task_4.js
@@ -7,7 +7,7 @@ const lengthInCm = parseFloat(prompt(`Довжина у сантиметрах`,
 // Обчислення результатів
 
 const lengthInM = lengthInCm * Math.pow(10,-2)
-const lengthInKm = lengthInCm * Math.pow(10,-3)
+const lengthInKm = lengthInCm * Math.pow(10,-5)
 
 // Виведення результатів
 
